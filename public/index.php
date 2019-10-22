f0ed9bb62b10fde249275121fad320ce57b8ab21
lisperow <lisperow@yandex.ru>
2019-10-22 19:35:11 +0300

    add index.phtml

--- public/index.php
+++ public/index.php
@@ -5,30 +5,19 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+$users = Slim\Example\Generator::generate(100);
+
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim');
-});
-
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
-});
-
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});
-
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-
-    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+    return $this->get('renderer')->render($response, 'index.phtml');
 });
 
 $app->run();
