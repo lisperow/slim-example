588947431b2acb8fb13082c16656c6403dce8d39
lisperow <lisperow@yandex.ru>
2019-10-24 12:26:29 +0300

    update generator

--- src/Generator.php
+++ src/Generator.php
@@ -6,35 +6,21 @@ class Generator
 {
     public static function generate($count)
     {
-        $range = range(1, $count - 2);
-        $numbers = collect($range)->shuffle(1)->toArray();
+        $numbers = range(1, $count - 2);
+        shuffle($numbers);
 
         $faker = \Faker\Factory::create();
-        $faker->seed(1234);
-        $users = [];
+        $faker->seed(1);
+        $posts = [];
         for ($i = 0; $i < $count - 2; $i++) {
-            $users[] = [
-                'id' => $numbers[$i],
-                'firstName' => $faker->firstName,
-                'lastName' => $faker->lastName,
-                'email' => $faker->email
+            $posts[] = [
+                'id' => $fuker->uuid,
+                'name' => $faker->text(70),
+                'body' => $faker->sentence,
+                'slug' => $faker->slug
             ];
         }
 
-        $users[] = [
-            'id' => 99,
-            'firstName' => $faker->firstName,
-            'lastName' => $faker->lastName,
-            'email' => $faker->email
-        ];
-
-        $users[] = [
-            'id' => 100,
-            'firstName' => $faker->firstName,
-            'lastName' => $faker->lastName,
-            'email' => $faker->email
-        ];
-
-        return $users;
+        return $posts;
     }
 }
