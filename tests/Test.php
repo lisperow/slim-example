62255cadb8e9cd3c30b28d3e88ed6a495110878c
lisperow <lisperow@yandex.ru>
2019-10-24 12:40:13 +0300

    update tests

--- tests/Test.php
+++ tests/Test.php
@@ -11,42 +11,44 @@ class Test extends TestCase
     public function setUp(): void
     {
         $this->client = new \GuzzleHttp\Client([
-            'base_uri' => 'http://localhost:8080'
+            'base_uri' => 'http://localhost:8080',
+            'cookies' => true
         ]);
     }
 
-    public function testUser()
+    public function testPosts()
     {
-        $response = $this->client->get('/users/1');
+        $this->client->get('/');
+        $response = $this->client->get('/posts');
         $body = $response->getBody()->getContents();
-        $this->assertNotEmpty($body);
+
+        $this->assertContains('Qui illo error nihil laborum vero', $body);
+        $this->assertNotContains('Dicta voluptas fuga totam reiciendis qui', $body);
+
+        $response2 = $this->client->get('/posts?page=2');
+        $body2 = $response2->getBody()->getContents();
+        $this->assertContains('?page=1', $body2);
+        $this->assertContains('?page=3', $body2);
+
+        $this->assertNotContains('Itaque quibusdam tempora velit porro ut velit soluta', $body2);
+        $this->assertContains('Porro amet laborum iure molestiae', $body2);
     }
 
-    public function testUser2()
+    public function testPost()
     {
-        $response = $this->client->get('/users/99');
+        $response = $this->client->get("/posts/0b13e52d-b058-32fb-8507-10dec634a07c");
         $body = $response->getBody()->getContents();
-
-        $this->assertContains('Grant', $body);
-        $this->assertContains('Cara', $body);
-        $this->assertContains('borer.nigel@gmail.com', $body);
+        $this->assertContains('Quam ipsam voluptatem cupiditate sed natus debitis voluptas.', $body);
     }
 
-    public function testUser3()
+    /**
+     * @expectedException \GuzzleHttp\Exception\ClientException
+     * @expectedExceptionMessage 404
+     */
+    public function testPostNotFound()
     {
-        $response = $this->client->get('/users/100');
-        $body = $response->getBody()->getContents();
-
-        $this->assertContains('Haley', $body);
-        $this->assertContains('Strokes', $body);
+        $this->client->get('/');
+        $this->client->get('/post/undefined');
     }
-
-    public function testUsers()
-    {
-        $response = $this->client->get('/users');
-        $body = $response->getBody()->getContents();
-
-        $this->assertContains('Julianne', $body);
-        $this->assertContains('Norval', $body);
     }
 }
