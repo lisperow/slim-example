7cc5b1b8af578e1427f1824c36126275c9798e9c
lisperow <lisperow@yandex.ru>
2019-10-25 12:13:22 +0300

    create Validator.php

---
+++ src/Validator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Slim\Example;
+
+class Validator
+{
+    public function validate(array $post)
+    {
+        $errors = [];
+        if ($post['name'] == '') {
+            $errors['name'] = "Can't be blank";
+        }
+
+        if (empty($post['body'])) {
+            $errors['body'] = "Can't be blank";
+        }
+
+        return $errors;
+    }
+}
